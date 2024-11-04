5da0db66631bee4cead6c7843c5377fc73bbe853
EternalAnime <eternalmelodymagic@gmail.com>
2024-11-04 14:04:10 +0530

    Update Overview.js

--- src/components/details/tabs/Overview.js
+++ src/components/details/tabs/Overview.js
@@ -2,8 +2,18 @@
 import React, { useState } from 'react'
 import styles from '../../../styles/AnimeDetailsBottom.module.css'
 import { Tooltip } from "@nextui-org/react";
+import { ShareIcon,PlayIcon } from "@heroicons/react/24/solid";
+import { AniListIcon,MyAnimeListIcon } from "@/lib/SvgIcons";
+import { useRouter } from 'next-nprogress-bar';
+import { Modal, ModalContent, ModalHeader, ModalBody, Button, useDisclosure } from "@nextui-org/react";
+import { useTitle } from '@/lib/store';
+import { useStore } from 'zustand';
 
 function Overview({data}) {
+    const animetitle = useStore(useTitle, (state) => state.animetitle);
+  const [openlist, setOpenlist] = useState(false);
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
     const [showFullDescription, setShowFullDescription] = useState(false);
 
     const toggleDescription = () => {
@@ -31,11 +41,54 @@ function Overview({data}) {
         return month[m]
     }
 
+    
+    const isAnime = data?.type === 'ANIME' || true;
+
+  
+    const handleShareClick = async () => {
+      try {
+        if (navigator.share) {
+          await navigator.share({
+            title: `${isAnime ? "Watch" : "Read"} Now - ${data?.title?.english}`,
+             text: `Watch [${data?.title?.romaji}] and more on 1Anime. Join us for endless anime entertainment`,
+            url: window.location.href,
+          });
+        } else {
+          // Web Share API is not supported, provide a fallback or show a message
+          alert("Web Share API is not supported in this browser.");
+        }
+      } catch (error) {
+        console.error("Error sharing:", error);
+      }
+    };
+    const router = useRouter();
+      // Assuming data.genres is an array of genre names
+  const hasHentaiGenre = data.genres.includes('Hentai');
+
+  if (hasHentaiGenre) {
+    // Redirect to the warning page
+    router.push('/warnings/nsfw');
+  }
+
+  // Check if "Action" or "Ecchi" is among the genres
+  const hasActionGenre = data.genres.includes("Action");
+  const hasEcchiGenre = data.genres.includes("Ecchi");
+
+  // Determine the age rating based on genres
+  const ageRating = hasActionGenre || hasEcchiGenre ? "16+" : "13+";
+
     return (
         <div className={styles.detailscard}>
             <div className={styles.card1}>
                 <h3 className={styles.detailsheading}>Details</h3>
+            
                 <div className={styles.detailscontent}>
+                <div className={styles.singlecontent}>
+                        <span className={styles.sideheading}>Age Rating</span> <span className={styles.con}>{ageRating}</span>
+                    </div>
+                <div className={styles.singlecontent}>
+                        <span className={styles.sideheading}>Romaji</span> <span className={styles.con}>{data?.title?.romaji}</span>
+                    </div>
                   {data?.status==='RELEASING' && 
                     <div className={styles.singlecontent}>
                         <span className={`${styles.sideheading} font-semibold !text-[15px]`}>Airing</span>
@@ -88,12 +141,70 @@ function Overview({data}) {
                         <span className={styles.sideheading}>Duration</span> <span className={styles.con}>{`${data?.duration} min` || `Na`}</span>
                     </div>
                     <div className={styles.singlecontent}>
-                        <span className={styles.sideheading}>Popularity</span> <span className={styles.con}>{`${data?.popularity} users`}</span>
+                        <span className={styles.sideheading}>Popularity (AniList)</span> <span className={styles.con}>{`${data?.popularity} users`}</span>
                     </div>
                 </div>
             </div>
             <div className={styles.card2}>
-                <h3 className={styles.detailsheading}>Description</h3>
+                <h3 className={styles.detailsheading}>Description</h3>   
+                <div className="flex space-x-4">
+        <a
+            type="button"
+            className="bg-[#1a1a1f] text-white text-xs font-bold px-2 py-1 rounded-md"
+            onClick={handleShareClick}
+          >
+            <span className="absolute pointer-events-none z-40 opacity-0 -translate-y-8 group-hover:-translate-y-10 group-hover:opacity-100 font-karla shadow-tersier shadow-md whitespace-nowrap bg-secondary px-2 py-1 rounded transition-all duration-200 ease-out">
+              Share {isAnime ? "Anime" : "Manga"}
+            </span>
+            <ShareIcon className="w-7 h-7" />
+          </a>
+      <a
+                            type="button"
+                            target="_blank"
+            rel="noopener noreferrer"
+                            href={`https://anilist.co/anime/${data.id}`}
+                            className="bg-[#1a1a1f] text-white text-xs font-bold px-2 py-1 rounded-md"
+                        >
+                            <span className="absolute pointer-events-none z-40 opacity-0 -translate-y-8 group-hover:-translate-y-10 group-hover:opacity-100 font-karla shadow-tersier shadow-md whitespace-nowrap bg-secondary px-2 py-1 rounded transition-all duration-200 ease-out">
+                                AniList
+                            </span>
+                            <AniListIcon className="w-7 h-7" /></a>
+
+<a
+                            type="button"
+                            target="_blank"
+            rel="noopener noreferrer"
+                            href={`https://myanimelist.net/anime/${data?.idMal}`}
+                            className="bg-[#1a1a1f] text-white text-xs font-bold px-2 py-1 rounded-md"
+                        >
+                            <span className="absolute pointer-events-none z-40 opacity-0 -translate-y-8 group-hover:-translate-y-10 group-hover:opacity-100 font-karla shadow-tersier shadow-md whitespace-nowrap bg-secondary px-2 py-1 rounded transition-all duration-200 ease-out">
+                                MAL
+                            </span>
+                            <MyAnimeListIcon className="w-7 h-7" /></a>
+                            <>
+        <a type="button" className="bg-[#1a1a1f] text-white text-xs font-bold px-2 py-1 rounded-md" onClick={onOpen}><PlayIcon className="w-7 h-7"/></a>
+        <Modal backdrop='blur' isOpen={isOpen} onOpenChange={onOpenChange} size={"2xl"} placement="center">
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-0">Trailer of {data.title?.[animetitle] || data?.title?.romaji}</ModalHeader>
+                <ModalBody>
+                  <div>
+                    <iframe
+                      title="Trailer"
+                      className='w-[320px] h-[150px] mb-4'
+                      src={`https://www.youtube.com/embed/${data?.trailer?.id}`}
+                      frameBorder="0"
+                      allowFullScreen
+                    ></iframe>
+                  </div>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </>
+      </div>
                 <div className={styles.descriptioncontent}>
                     <p dangerouslySetInnerHTML={{ __html: data?.description }} />
                 </div>
@@ -109,7 +220,7 @@ function Overview({data}) {
                         </button>
                     )}
                 </div>
-            </div>
+               </div>
         </div>
     )
 }
